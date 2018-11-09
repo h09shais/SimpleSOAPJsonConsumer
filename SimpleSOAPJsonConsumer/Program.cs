0c8f1eac26c0a30f72979faa4034d1168244a007
Shanawaz Islam <sis@nisportal.com>
2018-11-09 14:57:43 +0100

    Initial commit

---
+++ SimpleSOAPJsonConsumer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SimpleSOAPJsonConsumer.SimpleService;
+
+namespace SimpleSOAPJsonConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new SimpleServiceClient();
+            var response = client.Authenticate(new AuthenticationRequest
+            {
+                Username = "test",
+                Password = "test"
+            });
+
+            Console.WriteLine(response.Valid ? response.Message : "Error!");
+            Console.ReadLine();
+        }
+    }
+}
